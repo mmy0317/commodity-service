07173a589d505f8d74c9b220d9d8c5ea63975580
mayang <1659254531@qq.com>
2024-07-29 15:42:33 +0800

    feat: 基本业务逻辑示例的完善

---
+++ sale/core/target/generated-sources/annotations/com/mayang/convert/CommodityConfigConvertMapperImpl.java
@@ -0,0 +1,28 @@
+package com.mayang.convert;
+
+import com.mayang.dos.CommodityConfigDO;
+import com.mayang.dto.request.CreateCommodityDTO;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-07-29T15:10:48+0800",
+    comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_292 (AdoptOpenJDK)"
+)
+public class CommodityConfigConvertMapperImpl implements CommodityConfigConvertMapper {
+
+    @Override
+    public CommodityConfigDO CommodityConfigDoConvertFromDto(CreateCommodityDTO createCommodityDTO) {
+        if ( createCommodityDTO == null ) {
+            return null;
+        }
+
+        CommodityConfigDO commodityConfigDO = new CommodityConfigDO();
+
+        commodityConfigDO.setCommodityName( createCommodityDTO.getCommodityName() );
+        commodityConfigDO.setCommodityPrice( createCommodityDTO.getCommodityPrice() );
+        commodityConfigDO.setCommodityCount( createCommodityDTO.getCommodityCount() );
+
+        return commodityConfigDO;
+    }
+}
